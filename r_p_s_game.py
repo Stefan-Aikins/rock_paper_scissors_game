91f07a19e48d780f1b5b1e583a63b70785c63164
Kofi Aikins <stefanaikins@gmail.com>
2023-08-31 00:35:09 +0100

    update

--- r_p_s_game.py
+++ r_p_s_game.py
@@ -28,3 +28,35 @@ scissors = """
       (____)
 ---.__(___)
 """
+
+pick = input(
+    f"""rock {rock}   
+    
+    paper{paper}    
+    
+    scissors{scissors}   
+    Enter: [r] for rock   
+           [p] for paper   
+           [s] for scissors"
+    """
+).lower()
+
+while (pick != "r") or (pick != "p") or (pick != "s"):
+    pick = input(
+        f"""rock {rock}   
+    
+    paper{paper}    
+    
+    scissors{scissors}   
+    Enter: [r] for rock   
+           [p] for scissors    
+           [s] for scissors"
+    """
+    ).lower()
+
+# options to choose from
+options = ["r", "p", "s"]
+CPU = options[random.randint(0, len(options))]
+win_msg = "You win"
+loss_msg = "You lose"
+draw_msg = "its a draw"
